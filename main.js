77dfc112edb49e92ba819a19b0e2349838a03a58
Oscar Herrera <a.oscarherrera@gmail.com>
2023-11-06 21:12:32 -0600

    hamburger icon transitons to cross icon

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 const navMenu = document.querySelector('.nav-menu');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const crossMenu = document.querySelector('.cross-menu');
+
 
 hamburgerMenu.addEventListener('click', () => {
     navMenu.classList.toggle('hide');
-})
+    crossMenu.style.visibility = 'visible';
+    hamburgerMenu.style.visibility = 'hidden';
+});
+
+
+crossMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('hide');
+    crossMenu.style.visibility = 'hidden';
+    hamburgerMenu.style.visibility = 'visible';
+});
+
+
+const projectOne = document.querySelector('.pro-one');
+const projectTwo = document.querySelector('.pro-two');
+const projectThree = document.querySelector('.pro-three');
+const prevBtn = document.querySelector('.prev');
+const nxtBtn = document.querySelector('.next');
+
+
+
+
+
