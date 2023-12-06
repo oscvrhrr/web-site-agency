4a8899b6dabae691cd7e02075f5ba4979d423109
Oscar Herrera <a.oscarherrera@gmail.com>
2023-12-06 08:46:18 -0600

    added interactive projects

--- main.js
+++ main.js
@@ -16,12 +16,44 @@ crossMenu.addEventListener('click', () => {
     hamburgerMenu.style.visibility = 'visible';
 });
 
+let next = Array.from(document.getElementsByClassName('next'));
+let prev = Array.from(document.getElementsByClassName('prev'));
+let projects = document.getElementsByClassName('dynamic-project');
+let first = document.getElementsByClassName('first')[0];
+let second = document.getElementsByClassName('second')[0];
+let third = document.getElementsByClassName('third')[0];
+let currentState = 1;
 
-const projectOne = document.querySelector('.pro-one');
-const projectTwo = document.querySelector('.pro-two');
-const projectThree = document.querySelector('.pro-three');
-const prevBtn = document.querySelector('.prev');
-const nxtBtn = document.querySelector('.next');
+prev.forEach((button) => {
+    button.addEventListener('click', () => {
+  
+        if (currentState === 3) {
+            third.classList.add('hidden');
+            second.classList.remove('hidden')
+            currentState = 2;
+        } else if (currentState === 2) {
+            second.classList.add('hidden');
+            first.classList.remove('hidden');
+            currentState = 1;
+        } 
+        console.log(currentState)
+    });
+})
+
+next.forEach((button) => {
+    button.addEventListener('click', () => {
+  
+        if (currentState === 1) {
+            first.classList.add('hidden');
+            second.classList.remove('hidden')
+            currentState = 2;
+        } else if (currentState === 2) {
+            second.classList.add('hidden')
+            third.classList.remove('hidden');
+            currentState = 3;
+        } 
+    });
+})
 
 
 
